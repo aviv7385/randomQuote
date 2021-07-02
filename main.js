75d9124ca78f60a3a0e06273e755f1568a997520
AvivE <aviv7385@gmail.com>
2021-07-03 02:21:40 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+// get HTML elements 
+const quoteTextSpan = document.getElementById("quote");
+const quoteAuthorSpan = document.getElementById("author");
+const quoteContainer = document.getElementById("quote-container");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+// show loader
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loader
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// show new quote
+function newQuote() {
+    loading(); // show loader
+    // get a random number between 0 and the length of the apiQuotes array
+    const randomNumber = Math.floor(Math.random() * apiQuotes.length);
+    // a randomly chosen quote"
+    const quote = apiQuotes[randomNumber];
+
+    // get specific fields from the random quote 
+    const quoteText = quote.text;
+    const quoteAuthor = quote.author;
+
+    // insert quote data to the HTML elements
+    // check if author field is null and replace it with "unknown"
+    if (!quoteAuthor) {
+        quoteAuthorSpan.innerText = "Unknown";
+    }
+    else {
+        quoteAuthorSpan.innerText = quoteAuthor;
+    }
+
+    // check the quote's length to determine the styling
+    if (quoteText.length > 50) {
+        quoteTextSpan.classList.add("long-quote"); // if the text is too long - add a new class to the quote-text div
+    }
+    else {
+        quoteTextSpan.classList.remove("long-quote");
+    }
+
+    quoteTextSpan.innerText = quoteText;
+    complete(); // hide the loader
+}
+
+
+// get quotes from API
+async function getQuotes() {
+    loading(); // show loader
+    const apiUrl = "https://type.fit/api/quotes";
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+// tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteTextSpan.textContent} - ${quoteAuthorSpan.textContent}`;
+    window.open(twitterUrl, "_blank"); // open window in a new tab    
+}
+
+// on load
+getQuotes();
+
